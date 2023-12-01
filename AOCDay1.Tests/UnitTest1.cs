17f0a4dd946f032dcb63a24b638304cdf567c761
Pete Fitzpatrick <pete.fitzpatrick@hogarth.com>
2023-12-01 21:30:04 +0000

    part 2 but didn't work

--- AOCDay1.Tests/UnitTest1.cs
+++ AOCDay1.Tests/UnitTest1.cs
@@ -7,8 +7,15 @@ public class UnitTest1
     [Theory]
     [InlineData("zlmlk1", 11)]
     [InlineData("vqjvxtc79mvdnktdsxcqc1sevenone", 71)]
-    [InlineData("vsskdclbtmjmvrseven6", 66)]
+    [InlineData("vsskdclbtmjmvrseven6", 76)]
     [InlineData("8jkfncbeight7seven8", 88)]
+    [InlineData("two1nine", 29)]
+    [InlineData("eightwothree", 83)]
+    [InlineData("abcone2threexyz", 13)]
+    [InlineData("xtwone3four", 24)]
+    [InlineData("4nineeightseven2", 42)]
+    [InlineData("zoneight234", 14)]
+    [InlineData("7pqrstsixteen", 76)]
     public void Test1(string line, int expected)
     {
         Calibration.GetValue(line).ShouldBe(expected);
--- AOCDay1/Program.cs
+++ AOCDay1/Program.cs
@@ -12,7 +12,10 @@ internal class Program
         int sum = 0;
         foreach (var item in allLines)
         {
-            sum += Calibration.GetValue(item);
+            int value = Calibration.GetValue(item);
+            sum += value;
+
+            Console.WriteLine($"{item} {value} {sum}");
         }
 
         Console.WriteLine(sum);
@@ -24,11 +27,33 @@ public static class Calibration
 {
     public static int GetValue(string item)
     {
-        var matches = Regex.Matches(item, "\\d");
+        var matches = Regex.Matches(item, "(\\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)");
 
-        var first = matches.First().Value;
-        var last = matches.Last().Value;
+        var first = Mappings[matches.First().Value];
+        var last = Mappings[matches.Last().Value];
 
         return int.Parse($"{first}{last}");
     }
+
+    private static Dictionary<string, string> Mappings = new Dictionary<string, string>
+    {
+        ["one"]="1",
+        ["two"]="2",
+        ["three"]="3",
+        ["four"]="4",
+        ["five"]="5",
+        ["six"]="6",
+        ["seven"]="7",
+        ["eight"]="8",
+        ["nine"]="9",
+        ["1"]="1",
+        ["2"]="2",
+        ["3"]="3",
+        ["4"]="4",
+        ["5"]="5",
+        ["6"]="6",
+        ["7"]="7",
+        ["8"]="8",
+        ["9"]="9"
+    };
 }
