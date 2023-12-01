f08ca7b0626b43cd5ca7c07128fb1657643fa213
Pete Fitzpatrick <pete.fitzpatrick@hogarth.com>
2023-12-01 22:17:54 +0000

    regex doesn't work with overlapping words - eg. "twone"

--- AOCDay1.Tests/UnitTest1.cs
+++ AOCDay1.Tests/UnitTest1.cs
@@ -16,8 +16,27 @@ public class UnitTest1
     [InlineData("4nineeightseven2", 42)]
     [InlineData("zoneight234", 14)]
     [InlineData("7pqrstsixteen", 76)]
+    [InlineData("248twofbkfpxtheightwovng", 22)]
     public void Test1(string line, int expected)
     {
-        Calibration.GetValue(line).ShouldBe(expected);
+        Calibration.GetValue2(line).ShouldBe(expected);
+    }
+
+    [Theory]
+    [InlineData("zlmlk1", 11)]
+    [InlineData("vqjvxtc79mvdnktdsxcqc1sevenone", 71)]
+    [InlineData("vsskdclbtmjmvrseven6", 76)]
+    [InlineData("8jkfncbeight7seven8", 88)]
+    [InlineData("two1nine", 29)]
+    [InlineData("eightwothree", 83)]
+    [InlineData("abcone2threexyz", 13)]
+    [InlineData("xtwone3four", 24)]
+    [InlineData("4nineeightseven2", 42)]
+    [InlineData("zoneight234", 14)]
+    [InlineData("7pqrstsixteen", 76)]
+    [InlineData("248twofbkfpxtheightwovng", 22)]
+    public void Test2a(string line, int expected)
+    {
+        Calibration.GetValue2a(line).ShouldBe(expected);
     }
 }
--- AOCDay1/Program.cs
+++ AOCDay1/Program.cs
@@ -12,7 +12,7 @@ internal class Program
         int sum = 0;
         foreach (var item in allLines)
         {
-            int value = Calibration.GetValue(item);
+            int value = Calibration.GetValue2a(item);
             sum += value;
 
             Console.WriteLine($"{item} {value} {sum}");
@@ -27,6 +27,16 @@ public static class Calibration
 {
     public static int GetValue(string item)
     {
+        var matches = Regex.Matches(item, "\\d");
+
+        var first = Mappings[matches.First().Value];
+        var last = Mappings[matches.Last().Value];
+
+        return int.Parse($"{first}{last}");
+    }
+
+    public static int GetValue2(string item)
+    {
         var matches = Regex.Matches(item, "(\\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)");
 
         var first = Mappings[matches.First().Value];
@@ -35,25 +45,38 @@ public static class Calibration
         return int.Parse($"{first}{last}");
     }
 
+    public static int GetValue2a(string item)
+    {
+        var numbers = new [] {"1","2","3","4","5","6","7","8","9", "one","two","three","four","five","six","seven","eight","nine"};
+
+        var firstWords = numbers.Select(w => new {Word = w, Index = item.IndexOf(w)}).Where(w => w.Index != -1).OrderBy(w => w.Index).First();
+        var lastWords = numbers.Select(w => new {Word = w, Index = item.LastIndexOf(w)}).Where(w => w.Index != -1).OrderByDescending(w => w.Index).First();
+
+        var firstNumber = Mappings[firstWords.Word];
+        var lastNumber = Mappings[lastWords.Word];
+
+        return int.Parse($"{firstNumber}{lastNumber}"); 
+    }
+
     private static Dictionary<string, string> Mappings = new Dictionary<string, string>
     {
-        ["one"]="1",
-        ["two"]="2",
-        ["three"]="3",
-        ["four"]="4",
-        ["five"]="5",
-        ["six"]="6",
-        ["seven"]="7",
-        ["eight"]="8",
-        ["nine"]="9",
-        ["1"]="1",
-        ["2"]="2",
-        ["3"]="3",
-        ["4"]="4",
-        ["5"]="5",
-        ["6"]="6",
-        ["7"]="7",
-        ["8"]="8",
-        ["9"]="9"
+        ["one"] = "1",
+        ["two"] = "2",
+        ["three"] = "3",
+        ["four"] = "4",
+        ["five"] = "5",
+        ["six"] = "6",
+        ["seven"] = "7",
+        ["eight"] = "8",
+        ["nine"] = "9",
+        ["1"] = "1",
+        ["2"] = "2",
+        ["3"] = "3",
+        ["4"] = "4",
+        ["5"] = "5",
+        ["6"] = "6",
+        ["7"] = "7",
+        ["8"] = "8",
+        ["9"] = "9"
     };
 }
